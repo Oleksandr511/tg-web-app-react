22a1ff0ed42fcd7a38d27baa79ad052ff792d59d
Oleksandr <fjvffr@gmail.com>
2024-05-22 22:34:26 +0300

    use header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './components/hooks/useTelegram';
-
+// import header from './components/Header/Header';
+import Header from './components/Header/header';
 
 function App() {
 
-  const {onToggleButton, tg} = useTelegram()
+  const { onToggleButton, tg } = useTelegram()
 
   useEffect(() => {
     tg.ready()
-  },[])
+  }, [])
 
 
 
   return (
     <div className="App">
+      <Header/>
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTelegram } from '../../hooks/useTelegram'
+import '../Header/header.css'
 
 function header() {
     const {user,onClose} = useTelegram()
