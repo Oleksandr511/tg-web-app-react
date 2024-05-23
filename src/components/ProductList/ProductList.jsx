19d604ad30994b77ba8b5acaf91cffb1d39e2882
Oleksandr <fjvffr@gmail.com>
2024-05-23 18:01:06 +0300

    fix bug

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './ProductList.css'
 import ProductItem from '../ProductItem/ProductItem'
 import { useTelegram } from '../hooks/useTelegram'
+import { useState } from 'react'
 
 const products = [
   {id: '1', title: 'Jeans', price: 120, description: 'Blue color, streight'},
