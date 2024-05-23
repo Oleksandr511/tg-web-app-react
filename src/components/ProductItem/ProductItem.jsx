59af18e0a0d25bbe98e3d61701bce31e6395126b
Oleksandr <fjvffr@gmail.com>
2024-05-23 18:34:05 +0300

    fix bugs

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Button from '../Button/button.js'
-import './ProductItem.css'
+import React from 'react';
+import Button from "../Button/button";
+import './ProductItem.css';
 
 const ProductItem = ({product, className, onAdd}) => {
-    const onAddHandler = () => {
-        onAdd(product)
-    }
-  return (
-    <div className={'product' + className}>
-        <div className={'img'}/>
-        <div className={'title'}>{product.title}</div>
-        <div className={'description'}>{product.description}</div>
-        <div className={'price'}>
-            <span>Price: <b>{product.price}</b></span>
-        </div>
-        <Button className={'add-btn'} onClick={onAddHandler}>
-            Add to cart
-        </Button>
-    </div>
-  )
-}
 
-export default ProductItem
+    const onAddHandler = () => {
+        onAdd(product);
+    }
+
+    return (
+        <div className={'product ' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
+        </div>
+    );
+};
+
+export default ProductItem;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,59 +2,58 @@ import React from 'react'
 import './ProductList.css'
 import ProductItem from '../ProductItem/ProductItem.jsx'
 import { useTelegram } from '../hooks/useTelegram'
+
 import { useState } from 'react'
 
 const products = [
-  {id: '1', title: 'Jeans', price: 120, description: 'Blue color, streight'},
-  {id: '2', title: 'Jacket', price: 100, description: 'Green color, warm'},
-  {id: '3', title: 'cap', price: 20, description: 'Black color, modern'}
+  { id: '1', title: 'Jeans', price: 120, description: 'Blue color, streight' },
+  { id: '2', title: 'Jacket', price: 100, description: 'Green color, warm' },
+  { id: '3', title: 'cap', price: 20, description: 'Black color, modern' }
 ]
 
-const getTotalPrice = (items = [])=>{
-  return items.reduce((acc,cur)=>{
-    return acc+=cur.price
-  },0)
+const getTotalPrice = (items = []) => {
+  return items.reduce((acc, cur) => {
+    return acc += cur.price
+  }, 0)
 }
 
 
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([])
-  const {tg} = useTelegram()
+  const { tg } = useTelegram()
   const onAdd = (product) => {
-     const alreadyAdded = addedItems.find(item=>item.id === product.id)
-     let newItems = []
+    const alreadyAdded = addedItems.find(item => item.id === product.id)
+    let newItems = []
 
-     if (alreadyAdded) {
+    if (alreadyAdded) {
       newItems = addedItems.filter(item => item.id !== product.id)
-     } else {
+    } else {
       newItems = [...addedItems, product]
-     }
+    }
 
-     setAddedItems(newItems)
+    setAddedItems(newItems)
 
-     if(newItems.length === 0) {
+    if (newItems.length === 0) {
       tg.MainButton.hide()
-     } else {
+    } else {
       tg.MainButton.show()
       tg.MainButton.setParams({
         text: `Buy ${getTotalPrice(newItems)}`
       })
-     }
+    }
   }
 
   return (
     <div className={'list'}>
-      {products.map(el=>{
-        <ProductItem 
-          product={el}
-          onAdd={onAdd}
-          className={'item'}
-        />
-
-      })}
-        
+        {products.map(item => (
+            <ProductItem
+                product={item}
+                onAdd={onAdd}
+                className={'item'}
+            />
+        ))}
     </div>
-  )
+);
 }
 
 export default ProductList
